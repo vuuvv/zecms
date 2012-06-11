d91c9140cb0df6c2a25b103d62551d1c04414496
Zeng Ying <vuuvv@qq.com>
2012-06-11 18:02:59 +0800

    oop

---
+++ joyousite/joyouweb/Scripts/allin/remaind.js
@@ -0,0 +1,63 @@
+﻿!function ($) {
+
+    var create_or_call = function ($dom, name, option, args, cls) {
+        return $dom.each(function () {
+            var $this = $(this),
+        data = $this.data(name),
+        options = typeof option == 'object' && option;
+            if (!data) // object not created, now create
+                $this.data(name, (data = new cls(this, options)));
+            if (typeof option == 'string') // call method
+                data[option].apply(data, Array.prototype.slice.call(args, 1));
+        });
+    }
+
+    var Remaind = function (element, options) {
+        var self = this,
+        $element = this.$element = $(element),
+        options = this.options = $.extend({}, $.fn.remaind.defaults, options);
+        var a = this.calc();
+        self.set();
+        setInterval(function () {
+            self.set();
+        }, 60000);
+    };
+
+    Remaind.prototype = {
+        calc: function () {
+            var self = this,
+                $element = this.$element,
+                options = this.options,
+                seconds = Math.floor((options.deadline.getTime() - new Date().getTime()) / 1000),
+                day = Math.floor(seconds / 3600 / 24),
+                hour = Math.floor((seconds % (3600 * 24)) / 3600),
+                minite = Math.floor((seconds % 3600) / 60);
+
+            return {
+                day_0: Math.floor(day / 10),
+                day_1: day % 10,
+                hour_0: Math.floor(hour / 10),
+                hour_1: hour % 10,
+                minite_0: Math.floor(minite / 10),
+                minite_1: minite % 10
+            };
+        },
+        set: function () {
+            var obj = this.calc();
+            $("#remaind-day-0").text(obj.day_0);
+            $("#remaind-day-1").text(obj.day_1);
+            $("#remaind-hour-0").text(obj.hour_0);
+            $("#remaind-hour-1").text(obj.hour_1);
+            $("#remaind-minite-0").text(obj.minite_0);
+            $("#remaind-minite-1").text(obj.minite_1);
+        }
+    };
+
+    $.fn.remaind = function (option) {
+        create_or_call(this, "remaind", option, arguments, Remaind);
+    };
+
+    $.fn.remaind.defaults = {
+};
+
+} (jQuery);
---
+++ joyousite/joyouweb/allin/index.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/joyou.master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="joyouweb.allin.index" %>
+
+<asp:Content ContentPlaceHolderID="title" runat="server">All in JOYOU</asp:Content>
+<asp:Content ContentPlaceHolderID="head" runat="server">
+    <script src="/Scripts/jquery-1.4.4.min.js" type="text/javascript"></script>
+    <script src="/Scripts/allin/remaind.js" type="text/javascript"></script>
+    <link href="/Styles/allin/style.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+
+<asp:Content ContentPlaceHolderID="top_links" runat="server">
+    <span class="current-site">
+        <b>English</b>
+    </span>
+    <span class="links-pipe">|</span>
+    <a href="#">German</a>
+    <span class="links-pipe">|</span>
+    <a href="#">Chinese</a>
+    <span class="links-pipe">|</span>
+    <a href="#">Sitemap</a>
+</asp:Content>
+
+<asp:Content ContentPlaceHolderID="content" runat="server">
+    <div class="home-page">
+        <div class="home-main">
+            <a class="abs link home-video" href="video.aspx"></a>
+            <a class="abs link home-date" href="date.aspx"></a>
+            <a class="abs link home-history" href="history.aspx"></a>
+            <a class="abs link home-hot" href="hot.aspx"></a>
+            <a class="abs link home-activity" href="activity.aspx"></a>
+            <div class="abs remaind">
+                <div class="abs" id="remaind-day-0">&nbsp;</div>
+                <div class="abs" id="remaind-day-1">&nbsp;</div>
+                <div class="abs" id="remaind-hour-0">&nbsp;</div>
+                <div class="abs" id="remaind-hour-1">&nbsp;</div>
+                <div class="abs" id="remaind-minite-0">&nbsp;</div>
+                <div class="abs" id="remaind-minite-1">&nbsp;</div>
+            </div>
+            <script type="text/javascript">
+                $(".remaind").remaind({
+                    deadline: new Date(2012, 6, 28, 4, 12, 0)
+                });
+            </script>
+        </div>
+    </div>
+</asp:Content>
